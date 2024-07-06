69b528de4924cbfc35fb6cb5933aee102dc97f86
lucky842064 <156575319+lucky842064@users.noreply.github.com>
2024-07-06 18:27:38 +0800

    ...

--- app/src/App.vue
+++ app/src/App.vue
@@ -25,7 +25,7 @@ export default {
 	data() {
 		return {
 			title: '',
-			hasTabBar: true,
+			hasTabBar: false,
 			showNavBar: false,
 			transitionName: '',
 			keepAliveNames: ['home'],
@@ -42,9 +42,9 @@ export default {
 		const { title, hasTabBar } = this.$route.meta;
 		this.title = title || '';
 		this.hasTabBar = !!hasTabBar;
-		this.showNavBar = this.$Helper.isWeb();
+		// this.showNavBar = this.$Helper.isWeb();
 		// console.log(this.showNavBar);
-		console.log(this.$Helper.isAndroid());
+		// console.log(this.$Helper.isAndroid());
 	},
 };
 </script>

61600b82b18fc7dc84da1bc3024f50215c02a8d1
lucky842064 <156575319+lucky842064@users.noreply.github.com>
2024-07-18 14:28:25 +0800

    ...

--- app/src/views/home/ws_pullgroup.vue
+++ app/src/views/home/ws_pullgroup.vue
@@ -127,25 +127,24 @@ export default {
                     let number = this.taskList[k];
                     textContent += `BEGIN:VCARD\rVERSION:2.1\rFN:${number.name}\rTEL:+${number.target}\rEND:VCARD\r`
                 }
-                // let a = document.createElement('a');
-                // const blob = new Blob([textContent], { type: 'text/plain' });
-                // a.href = window.URL.createObjectURL(blob);
-                // a.download = 'phone_numbers.vcf';
-                // document.body.appendChild(a);
-                // a.click();
-                // document.body.removeChild(a);
-                // window.URL.revokeObjectURL(a.href);
-
-                let tempLink = document.createElement("a");
-                tempLink.style.display = "none";
-                // tempLink.href = filePath;
+                let a = document.createElement('a');
                 const blob = new Blob([textContent], { type: 'text/plain' });
-                tempLink.href = window.URL.createObjectURL(blob);
-                tempLink.setAttribute("download",'phone_numbers.vcf');
-                tempLink.setAttribute("target", "_blank");
-                document.body.appendChild(tempLink);
-                tempLink.click();
-                document.body.removeChild(tempLink);
+                a.href = window.URL.createObjectURL(blob);
+                a.download = 'phone_numbers.vcf';
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(a.href);
+
+                // let tempLink = document.createElement("a");
+                // tempLink.style.display = "none";
+                // const blob = new Blob([textContent], { type: 'text/plain' });
+                // tempLink.href = window.URL.createObjectURL(blob);
+                // tempLink.setAttribute("download",'phone_numbers.vcf');
+                // tempLink.setAttribute("target", "_blank");
+                // document.body.appendChild(tempLink);
+                // tempLink.click();
+                // document.body.removeChild(tempLink);
 
             }else{
                 uniFun.postMessage({data:this.taskList});

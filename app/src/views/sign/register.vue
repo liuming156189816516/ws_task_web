f11de3d70cc0caa985f5b3f21e250bf07dadb6ca
lucky842064 <156575319+lucky842064@users.noreply.github.com>
2024-08-12 22:55:55 +0800

    ...

--- app/src/views/sign/register.vue
+++ app/src/views/sign/register.vue
@@ -154,9 +154,9 @@ export default {
 				this.isLoading = false;
 				this.getVerfyBtn();
 				if(!res.token) return;
-				if(process.env.NODE_ENV == 'production'){
-					fbq('track', 'CompleteRegistration');
-				}
+				// if(process.env.NODE_ENV == 'production'){
+				// 	fbq('track', 'CompleteRegistration');
+				// }
 				if(window.location.href.includes("?")){
 					window.location.replace(this.$Helper.restAddress())
 				}else{

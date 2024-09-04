86f4d3d1814cc88369bb8d2a9eb87a7c78ed051e
lucky842064 <156575319+lucky842064@users.noreply.github.com>
2024-09-04 11:49:54 +0200

    ...

--- mex_app/src/views/Home.vue
+++ mex_app/src/views/Home.vue
@@ -79,7 +79,7 @@
             </div>
             <div class="adv_warp flex-item flex-between font_28">
                 <div class="adv_item flex-item flex-align flex-center" @click="jumpLucky(0)">
-                    {{$t('home_124')}}{{appLogin}}
+                    {{$t('home_124')}}{{$Helper.isMobileBrowser()}}
                 </div>
                 <div class="adv_item flex-item flex-align flex-center" @click="jumpLucky(1)">
                     {{$t('home_125')}}

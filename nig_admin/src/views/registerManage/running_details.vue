d87280859578b139e9dca9dcba4c2c0bc3e1b9b2
lucky842064 <156575319+lucky842064@users.noreply.github.com>
2024-12-08 00:05:33 +0100

    ...

--- nig_admin/src/views/registerManage/running_details.vue
+++ nig_admin/src/views/registerManage/running_details.vue
@@ -6,7 +6,7 @@
                 <el-input clearable :placeholder="$t('sys_mat061',{value:$t('sys_m068')})" v-model="l_account" />
             </el-form-item>
             <el-form-item>
-                <el-date-picker v-model="task_time" type="daterange" :clearable="false" :picker-options="pickerOptions" :range-separator="$t('sys_c108')" :start-placeholder="$t('sys_c109')" :end-placeholder="$t('sys_c110')" />
+                <el-date-picker v-model="task_time" type="daterange" :clearable="false" :range-separator="$t('sys_c108')" :start-placeholder="$t('sys_c109')" :end-placeholder="$t('sys_c110')" />
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" icon="el-icon-search" @click="initBillList(1)">{{ $t('sys_c002') }}</el-button>

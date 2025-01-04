e95a423c7f29113cc70f6577dce3977bf2e84def
lucky842064 <156575319+lucky842064@users.noreply.github.com>
2025-01-04 17:36:22 +0100

    ...

--- nig_app/src/App.vue
+++ nig_app/src/App.vue
@@ -164,7 +164,7 @@ body,
 		position: fixed;
 		top: 0;
 		left: 0;
-		background: rgba($color:$color-theme, $alpha: .7);
+		background: rgba($color:$color-theme, $alpha: .9);
 	}
 	.app_top_continer, .app_continer{
 		width: 100%;

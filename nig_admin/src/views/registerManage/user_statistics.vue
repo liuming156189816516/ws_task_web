45767378b3b0a95a9983d5f420945aac1bcdeba6
lucky842064 <156575319+lucky842064@users.noreply.github.com>
2025-01-09 10:49:25 +0100

    ...

--- nig_admin/src/views/registerManage/user_statistics.vue
+++ nig_admin/src/views/registerManage/user_statistics.vue
@@ -152,7 +152,7 @@ export default {
             statisticsList:[],
             accountDataList:[],
             pageOption: resetPage(),
-            showNum: [1,2,3,4,5,6,7,8,9,10]
+            showNum: [1,2,3,4,5,6,7,8]
         }
     },
     computed: {

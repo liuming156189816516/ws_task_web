65fea90f92ee36b5113bd1f6848ded32ea8e2581
lucky842064 <156575319+lucky842064@users.noreply.github.com>
2025-01-13 14:53:50 +0100

    ...

--- bra_admin/src/views/registerManage/user_statistics.vue
+++ bra_admin/src/views/registerManage/user_statistics.vue
@@ -150,7 +150,7 @@ export default {
             statisticsList:[],
             accountDataList:[],
             pageOption: resetPage(),
-            showNum: [1,2,3,4,5,6,7,8,13,14]
+            showNum: [1,2,3,4,5,6,7,8,12,13,14]
         }
     },
     computed: {

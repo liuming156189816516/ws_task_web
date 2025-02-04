b877406640588c743de4ad3d5224f39514e1e2b0
lucky842064 <156575319+lucky842064@users.noreply.github.com>
2025-02-04 11:24:56 +0100

    ...

--- nig_app/src/views/Home.vue
+++ nig_app/src/views/Home.vue
@@ -110,7 +110,7 @@
                                 <img v-for="(v,i) in taskNameOption[item.type].live2" :key="i" src="@/assets/images/gold_icon.png">
                             </div>
                             <div class="task_live_2 flex-item flex-align" v-else-if="item.type==1">
-                                <span class="flex-item">{{$t('other_099')}}{{taskNameOption[item.type].g_num}}</span>
+                                <span class="flex-item">{{$t('other_101')}}{{taskNameOption[item.type].g_num}}</span>
                                 <img v-for="(v,i) in taskNameOption[item.type].live2" :key="i" src="@/assets/images/gold_icon.png">
                             </div>
                             <div class="task_live_2 flex-item flex-align" v-else>
@@ -161,7 +161,7 @@ export default {
             wsList: [],
             tgList: [],
             langIdx:Cookies.get("language"),
-            taskType: ['', 'hookTask', 'codeOnline', 'pullgroupTask','pulledTask','telegTask','tgManualTask','tgGroupTask'],
+            taskType: ['', 'hookTask', 'highTask', 'pullgroupTask','pulledTask','telegTask','tgManualTask','tgGroupTask'],
             bannerList:[],
             imagesList:[
                 {
@@ -181,8 +181,7 @@ export default {
             return [
                 {},
                 {name:this.$t('home_168'),live1:1,live2:1,type:1,g_num:20, status:null,task_info_id:null,award:this.$t('home_168'),btn:this.$t('home_005'),desc:this.$t('home_051')},
-                {name:this.$t('home_145'),live1:2,live2:1,type:2,g_num:20,status:null,task_info_id:null,award:this.$t('home_139'),btn:this.$t('home_056'),desc:this.$t('home_049')},
-                // {name:this.$t('home_183'),live1:2,live2:1,type:2,g_num:30,status:null,task_info_id:null,award:this.$t('home_178'),btn:this.$t('home_057'),desc:this.$t('home_050')},
+                {name:this.$t('home_183'),live1:2,live2:1,type:2,g_num:30,status:null,task_info_id:null,award:this.$t('home_178'),btn:this.$t('home_057'),desc:this.$t('home_050')},
                 {name:this.$t('home_044'),live1:2,live2:1,type:3,g_num:15,status:null,task_info_id:null,award:this.$t('home_139'),btn:this.$t('home_056'),desc:this.$t('home_049')},
                 {name:this.$t('home_134'),live1:2,live2:1,type:4,g_num:2,status:null,task_info_id:null,award:this.$t('home_136'),btn:this.$t('home_005'),desc:this.$t('home_049')},
                 // {name:this.$t('home_145'),live1:2,live2:1,type:5,g_num:20,status:null,task_info_id:null,award:this.$t('home_139'),btn:this.$t('home_056'),desc:this.$t('home_049')},

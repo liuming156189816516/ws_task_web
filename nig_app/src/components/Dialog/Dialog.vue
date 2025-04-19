142d248774883ac27e7fc81b8464a756d8114068
lucky842064 <156575319+lucky842064@users.noreply.github.com>
2025-04-19 15:13:46 +0200

    ...

--- nig_app/src/components/Dialog/Dialog.vue
+++ nig_app/src/components/Dialog/Dialog.vue
@@ -183,7 +183,7 @@
                         </div>
                         <!-- <h4 class="w_f font_32">产生的速度慢了点吗没事没事面对吗没什么输出吗</h4> -->
                         <h4 class="w_f font_32">{{ title }}</h4>
-                        <div class="adv_content w_f flex-item flex-center flex-dir-c font_26" v-html="content"></div>
+                        <div class="adv_content w_f flex-item flex-dir-c font_26" v-html="content"></div>
                     </div>
                 </div>
             </div>
@@ -337,8 +337,8 @@ export default {
             width: 100%;
             flex-shrink: 0;
             overflow: hidden;
-            overflow-y: auto;
-            overflow-x: hidden;
+            // overflow-y: auto;
+            // overflow-x: hidden;
             min-height: 300px;
             max-height: 1200px;
             border-radius: 6px;
@@ -372,9 +372,9 @@ export default {
                 box-sizing: border-box;
             }
             .adv_content{
-                // min-height: 300px;
-                // max-height: 1200px;
-                // overflow-y: auto;
+                min-height: 300px;
+                max-height: 1200px;
+                overflow-y: auto;
                 transition: opacity 0.3s ease, visibility 0s linear 0.3s;
                 p{
                     width: 100%;
